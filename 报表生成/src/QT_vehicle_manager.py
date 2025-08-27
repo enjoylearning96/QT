82f9397fcde4a98992427c6c39d6991f4f7beaea
enjoylearning96 <148044540+enjoylearning96@users.noreply.github.com>
2025-08-27 23:45:21 +0800

    增加新增车辆时对数据的验证

--- 报表生成/src/QT_vehicle_manager.py
+++ 报表生成/src/QT_vehicle_manager.py
@@ -2,13 +2,14 @@
 Author: 李晓乐
 Date: 2025-08-23 11:41:24
 LastEditors: enjoylearning96 148044540+enjoylearning96@users.noreply.github.com
-LastEditTime: 2025-08-27 22:54:40
+LastEditTime: 2025-08-27 23:43:15
 FilePath: \QT\报表生成\src\QT_vehicle_manager.py
 Description: 
 
 Copyright (c) 2025 by ${git_name_email}, All Rights Reserved. 
 '''
 import sys
+import re
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QListWidgetItem, QMessageBox,
                             QInputDialog, QCheckBox)
 from PyQt6.QtCore import Qt
@@ -38,6 +39,8 @@ class VehicleManager(QMainWindow):
         if vehicle_manager_action=="增加车辆" :
             self.ui_vehicle.comboBox_3.hide()
             self.ui_vehicle.lineEdit.show()
+            self.ui_vehicle.lineEdit_2.setText("")
+            self.ui_vehicle.lineEdit_3.setText("")
         else:
             self.ui_vehicle.lineEdit.hide()
             self.type_changed(self.ui_vehicle.comboBox_2.currentText())
@@ -46,11 +49,14 @@ class VehicleManager(QMainWindow):
             
     # 根据车辆类型刷新车辆列表
     def type_changed(self, text):
-        vehicles = self.database.get_vehicle_data(vehicle_type=text)
-        self.ui_vehicle.comboBox_3.clear()
-        self.ui_vehicle.comboBox_3.addItems([str(v['vehicle_number']) for v in vehicles])
-        self.vehicle_number_changed(self.ui_vehicle.comboBox_3.currentText())
-        self.ui_vehicle.comboBox_3.currentTextChanged.connect(self.vehicle_number_changed)
+        if self.ui_vehicle.comboBox.currentText()=="增加车辆":
+            pass
+        else:
+            vehicles = self.database.get_vehicle_data(vehicle_type=text)
+            self.ui_vehicle.comboBox_3.clear()
+            self.ui_vehicle.comboBox_3.addItems([str(v['vehicle_number']) for v in vehicles])
+            self.vehicle_number_changed(self.ui_vehicle.comboBox_3.currentText())
+            self.ui_vehicle.comboBox_3.currentTextChanged.connect(self.vehicle_number_changed)
     
     def vehicle_number_changed(self, text):
         vehicle_data=self.database.get_vehicle_data(vehicle_number=text)
@@ -72,14 +78,19 @@ class VehicleManager(QMainWindow):
     def vehicle_add(self):
         vehicle_exists=self.database.get_vehicle_data(vehicle_number=self.ui_vehicle.lineEdit.text(), 
                                                                     vehicle_type=self.ui_vehicle.comboBox_2.currentText())
-        if not vehicle_exists:
-            self.database.insert_vehicle_data(vehicle_number=self.ui_vehicle.lineEdit.text(), 
-                                              vehicle_type=self.ui_vehicle.comboBox_2.currentText(), 
-                                              vehicle_ip=self.ui_vehicle.lineEdit_2.text(), 
-                                              load_capacity=self.ui_vehicle.lineEdit_3.text())
-        else :
-            QMessageBox.warning(self, "警告", "车辆已存在，无法添加！")
-            
+        if self.validate_ip_address(self.ui_vehicle.lineEdit_2.text()):
+            if self.validate_load_weight(weight=self.ui_vehicle.lineEdit_3.text()):
+                if self.ui_vehicle.lineEdit.text()!="":
+                    if not vehicle_exists:
+                        self.database.insert_vehicle_data(vehicle_number=self.ui_vehicle.lineEdit.text(), 
+                                                        vehicle_type=self.ui_vehicle.comboBox_2.currentText(), 
+                                                        vehicle_ip=self.ui_vehicle.lineEdit_2.text(), 
+                                                        load_capacity=self.ui_vehicle.lineEdit_3.text())
+                    else :
+                        QMessageBox.warning(self.ui_vehicle, "警告", "车辆已存在，无法添加！")
+                else:
+                    QMessageBox.warning(self.ui_vehicle, "警告", "车辆编号不能为空！")
+
     # 修改车辆
     def vehicle_update(self):
         self.database.update_vehicle_data(vehicle_number=self.ui_vehicle.comboBox_3.currentText(), 
@@ -89,3 +100,38 @@ class VehicleManager(QMainWindow):
     # 删除车辆
     def vehicle_delete(self):
         self.database.delete_vehicle_data(vehicle_number=self.ui_vehicle.comboBox_3.currentText())
+    
+    def validate_ip_address(self,ip):
+        """检测IP地址是否符合规范"""
+        # IP地址正则表达式
+        ip_pattern = r'^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$'
+        
+        match = re.match(ip_pattern, ip)
+        if not match:
+            QMessageBox.warning(self.ui_vehicle, "警告", "IP地址格式不正确")
+            return False
+        
+        # 检查每个数字段是否在0-255范围内
+        for segment in match.groups():
+            if not 0 <= int(segment) <= 255:
+                QMessageBox.warning(self.ui_vehicle, "警告", "IP超出范围(0-255)")
+                return False
+        
+        return True
+
+    def validate_load_weight(self, weight, min_weight=0, max_weight=200):
+        """检测载重量是否为数字且在合理范围内"""
+        try:
+            # 尝试转换为浮点数
+            weight_value = float(weight)
+            
+            # 检查范围
+            if not min_weight <= weight_value <= max_weight:
+                QMessageBox.warning(self.ui_vehicle, "警告", "载重量超出范围(0-200)")
+                return False
+            else:
+                return True
+            
+        except (ValueError, TypeError):
+            QMessageBox.warning(self.ui_vehicle, "警告", "载重量必须是数字")
+            return False
